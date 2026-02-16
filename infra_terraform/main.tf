d76bd935bfab2c102e4e511c71df1b431ad381ac
indarg <indarg@gmail.com>
2026-02-16 22:18:45 +0000

    fix: removiendo api_version y usando referencia directa de imagen

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "docker" {
   host = "unix:///var/run/docker.sock"
-  # ESTA ES LA LÍNEA MÁGICA: Forzamos a Terraform a hablar en 1.41
-  api_version = "1.41"
 }
 
 resource "docker_image" "postgres_image" {
@@ -19,9 +17,9 @@ resource "docker_image" "postgres_image" {
 }
 
 resource "docker_container" "postgres_container" {
-  # Usamos el nombre directamente para evitar el error de metadata
-  image = docker_image.postgres_image.name 
-  name  = "db_final_bariloche"
+  # REFERENCIA DIRECTA: Usamos el nombre de la imagen tal cual
+  image = "postgres:15-alpine" 
+  name  = "db_final_bariloche_v2"
 
   ports {
     internal = 5432
